931843498307d248624ff30c242aca2ca94d65f8
Harm Kolvoort <harm.kolvoort@ziggo.nl>
2014-11-15 15:06:24 +0100

    Added comments

--- Euler/src/hkol/P1_SumOfMultiples.java
+++ Euler/src/hkol/P1_SumOfMultiples.java
@@ -1,12 +1,35 @@
 package hkol;
 
+/**
+ * @author Harm Kolvoort
+ * @date 14-11-2014
+ * 
+ * Problem 1 Multiples of 3 and 5
+ * If we list all the natural numbers below 10 
+ * that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ * The sum of these multiples is 23.
+ * 
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ */
 public class P1_SumOfMultiples {
 	private Integer sum;
 	
+	/**
+	 * Constructor sets sum to zero. 
+	 */
 	public P1_SumOfMultiples() {
 		sum = 0;
 	}
 	
+	/**
+	 * Returns true if number is a multiple of divider
+	 * Example 1: 6 (number) is multiple of 3 (divider)
+	 * Example 2: 8 (number) not multiple of 5 (divider)
+	 * @param number
+	 * @param divider
+	 * @return
+	 */
 	public boolean isMultiple(Integer number, Integer divider) {
 		if (number % divider == 0) {
 			return true;

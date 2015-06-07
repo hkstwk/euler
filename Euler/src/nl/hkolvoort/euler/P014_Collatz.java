8e3f80a7402fd8256faa0bbee3d09bb559576d80
Harm Kolvoort <harm.kolvoort@ziggo.nl>
2015-06-07 12:20:38 +0200

    Project Euler problem 14 Collatz sequence. Still got issues.

---
+++ Euler/src/nl/hkolvoort/euler/P014_Collatz.java
@@ -0,0 +1,69 @@
+package nl.hkolvoort.euler;
+
+import java.util.ArrayList;
+
+/**
+ * @author harm
+ * @date 7-June-2015
+ * 
+ * Project Euler, Problem 14 Largest Collatz Sequence under 1 million
+ * 
+ */
+public class P014_Collatz {
+	
+	private ArrayList<Integer> collatzSequence;
+	
+	public P014_Collatz(Integer n){
+		collatzSequence = fillCollatzSequence(n);
+	}
+	
+	public ArrayList<Integer> fillCollatzSequence(int n){
+		ArrayList<Integer> sequence = new ArrayList<Integer>();
+		
+		sequence.add(n);
+		while (n>1){
+			if (n % 2 == 0){
+				n /= 2;
+			} 
+			else {
+				n = 3*n + 1;
+			}
+			sequence.add(n);
+		}
+		
+		return sequence;
+	}
+	
+	public String getCollatzSequence(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("Chain length = " + collatzSequence.size() + "\n");
+		for (int i=0; i<collatzSequence.size();i++){
+			sb.append(collatzSequence.get(i));
+			if (i<collatzSequence.size()-1){
+				sb.append(" - ");
+			}
+		}
+		return sb.toString();
+	}
+	
+	public int getLength(){
+		return collatzSequence.size();
+	}
+	
+
+	public static void main(String[] args) {
+		Integer largestCollatz = 0;
+		int maxLength = 1;
+		
+		for (Integer i=1000000; i>0; i--){
+			P014_Collatz col = new P014_Collatz(i);
+			if (col.getLength() > maxLength){
+				largestCollatz = i;
+				maxLength = col.getLength();
+				System.out.println(new P014_Collatz(largestCollatz).getCollatzSequence());
+			}
+			col = null;
+		}
+
+	}
+}

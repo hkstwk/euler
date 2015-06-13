109575f642ed644ce99b3a1dddc1147f0294af64
Harm Kolvoort <harm.kolvoort@ziggo.nl>
2015-06-13 20:21:24 +0200

    Problem 19 Counting Sundays initial empty class

---
+++ Euler/src/nl/hkolvoort/euler/P019_CountingSundays.java
@@ -0,0 +1,20 @@
+package nl.hkolvoort.euler;
+
+/**
+ * @author Harm
+ * @date 13-June-2015
+ * 
+ * How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+ *
+ */
+public class P019_CountingSundays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

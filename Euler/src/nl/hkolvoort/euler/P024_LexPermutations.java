e640a49f4c2ea125d1345ef95dc57b2c4221540c
Harm Kolvoort <hkolvoort+github@gmail.com>
2015-06-29 22:06:51 +0200

    Skeloton class for Problem 24 Lexicographic Permutations

---
+++ Euler/src/nl/hkolvoort/euler/P024_LexPermutations.java
@@ -0,0 +1,40 @@
+package nl.hkolvoort.euler;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author harm
+ * @date 29-June-2015
+ * 
+ * Problem 24 Lexicographic Permutations
+ * 
+ * A permutation is an ordered arrangement of objects. For example, 3124 is one 
+ * possible permutation of the digits 1, 2, 3 and 4. If all of the permutations 
+ * are listed numerically or alphabetically, we call it lexicographic order. The 
+ * lexicographic permutations of 0, 1 and 2 are:
+ * 
+ * 012   021   102   120   201   210
+ * 
+ * What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 
+ * 5, 6, 7, 8 and 9?
+ * 
+ */
+public class P024_LexPermutations {
+	
+	private static final List<Integer> digits = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
+		
+
+
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+	}
+
+}

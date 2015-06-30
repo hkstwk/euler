d9a96b981c7cab179eee853fd4f610486f2d816a
Harm Kolvoort <hkolvoort+github@gmail.com>
2015-06-30 21:44:20 +0200

    Solution to probem 24 Lexicographic permutations. basically scooped the solution from www.nayuki.io and wikipedia. In the end to lazy to code the algoritm myself. Sorry...

--- Euler/src/nl/hkolvoort/euler/P024_LexPermutations.java
+++ Euler/src/nl/hkolvoort/euler/P024_LexPermutations.java
@@ -1,13 +1,7 @@
 package nl.hkolvoort.euler;
 
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * @author harm
@@ -28,13 +22,54 @@ import java.util.List;
  */
 public class P024_LexPermutations {
 	
-	private static final List<Integer> digits = Arrays.asList(0,1,2,3,4,5,6,7,8,9);
-		
+	// source: http://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+	static boolean nextPermutation(int[] array) {
+	    // Find longest non-increasing suffix
+	    int i = array.length - 1;
+	    while (i > 0 && array[i - 1] >= array[i])
+	        i--;
+	    // Now i is the head index of the suffix
+	    
+	    // Are we at the last permutation already?
+	    if (i <= 0)
+	        return false;
+	    
+	    // Let array[i - 1] be the pivot
+	    // Find rightmost element that exceeds the pivot
+	    int j = array.length - 1;
+	    while (array[j] <= array[i - 1])
+	        j--;
+	    // Now the value array[j] will become the new pivot
+	    // Assertion: j >= i
+	    
+	    // Swap the pivot with j
+	    int temp = array[i - 1];
+	    array[i - 1] = array[j];
+	    array[j] = temp;
+	    
+	    // Reverse the suffix
+	    j = array.length - 1;
+	    while (i < j) {
+	        temp = array[i];
+	        array[i] = array[j];
+	        array[j] = temp;
+	        i++;
+	        j--;
+	    }
+	    
+	    // Successfully computed the next permutation
+	    return true;
+	}
 
 
 
 	public static void main(String[] args) throws FileNotFoundException {
-
+		int[] array = {0, 1, 2, 3, 4,5, 6, 7, 8, 9};  // Must start at lowest permutation
+		int count = 0;
+		do {
+		    System.out.println(Arrays.toString(array));
+		    count++;
+		} while (nextPermutation(array) && count < 1000000);
 	}
 
 }
